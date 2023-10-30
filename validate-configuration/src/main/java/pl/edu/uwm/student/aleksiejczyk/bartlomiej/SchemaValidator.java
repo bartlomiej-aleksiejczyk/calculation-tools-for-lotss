b27a4a1ef557c0821ec3a304910bb46dc7c7b056
bartlomiej-aleksiejczyk <bartlomiej.aleksiejczyk@gmail.com>
2023-10-30 07:41:16 +0100

    Change configuration file location to apps root directory

--- validate-configuration/src/main/java/pl/edu/uwm/student/aleksiejczyk/bartlomiej/SchemaValidator.java
+++ validate-configuration/src/main/java/pl/edu/uwm/student/aleksiejczyk/bartlomiej/SchemaValidator.java
@@ -11,12 +11,12 @@ import java.io.File;
 
 public class SchemaValidator {
     static final String SCHEMA_FILE_PATH = "./schema.xsd";
-    static final String CONFIG_FILE_NAME = "configuration.xml";
+    static final String CONFIG_FILE_NAME = "./configuration.xml";
 
     public static Boolean validateXMLAgainstXSDSchema() {
         try {
             File schemaFile = new File(SCHEMA_FILE_PATH);
-            File configurationFile = new File(DirectoryTools.getParentDirectory(), CONFIG_FILE_NAME);
+            File configurationFile = new File(CONFIG_FILE_NAME);
 
             Schema schema = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI).newSchema(schemaFile);
             Validator validator = schema.newValidator();
